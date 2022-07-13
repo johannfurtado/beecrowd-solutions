8bd8b80591419804070a9d151b2f8bc9c2c5860f
Johann Matos Furtado <61987811+johannfurtado@users.noreply.github.com>
2022-07-13 00:26:15 -0300

    Create 1005.java

---
+++ 1005.java
@@ -0,0 +1,31 @@
+/*
+ QUESTÃO
+ Leia 2 valores de ponto flutuante de dupla precisão A e B, que 
+ correspondem a 2 notas de um aluno. A seguir, calcule a média do aluno,
+ sabendo que a nota A tem peso 3.5 e a nota B tem peso 7.5 (A soma dos 
+ pesos portanto é 11). Assuma que cada nota pode ir de 0 até 10.0, sempre 
+ com uma casa decimal.
+ ENTRADA
+ O arquivo de entrada contém 2 valores com uma casa decimal cada um.
+ SAÍDA
+ Calcule e imprima a variável MEDIA conforme exemplo abaixo, com 5 dígitos
+ após o ponto decimal e com um espaço em branco antes e depois da igualdade.
+ Utilize variáveis de dupla precisão (double) e como todos os problemas, não
+ esqueça de imprimir o fim de linha após o resultado, caso contrário, você
+ receberá "Presentation Error".
+*/
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner ler = new Scanner(System.in);
+		double a = ler.nextDouble();
+		double b = ler.nextDouble();
+		double media = ((a*3.5) + (b*7.5))/11;
+		
+		System.out.println(String.format("MEDIA = %.5f" , media));
+	}
+}
